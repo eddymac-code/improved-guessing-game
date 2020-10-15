523450aa4e4f1f4ac40076e250fd28144cff9d2f
Eddy Mac <eddymac11@outlook.com>
2020-10-15 13:00:23 +0300

    Added model

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-
+  $oldguess = isset($_POST['guess']) ? $_POST['guess'] : '';
+  $message = false;
+  
+  if (!isset($_POST['guess'])) {
+    $message = "Please make a guess";
+  } elseif ($_POST['guess'] == "") {
+    $message = "You did not make a guess";
+  } elseif (!is_numeric($_POST['guess'])) {
+    $message = "Your guess is not a number";
+  } elseif ($_POST['guess'] < 36) {
+    $message = "Your guess is too low";
+  } elseif ($_POST['guess'] > 36) {
+    $message = "Your guess is too high";
+  } else {
+    $message = "Congratulations - You are right";
+  }
 ?>
 
 <html>
@@ -7,10 +22,16 @@
 <p>You need to guess a number. If correct, the application
 will give a message below.</p>
 
+<?php
+if ($message !== false) {
+  echo "<p>$message</p>\n"; 
+}
+?>
+
 <form method="post">
 <p><label for="guess">Enter your guess</label>
-<input type="text" name="guess" id="guess">
+<input type="text" name="guess" id="guess"
+value="<?= htmlentities($oldguess) ?>"></p>
 <input type="submit" value="Guess">
-</p>
 </form>
 </html>
