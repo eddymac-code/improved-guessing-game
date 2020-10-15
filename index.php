8ac375813abc7088a1d8ab4bfa7bbccc11d69b45
Eddy Mac <eddymac11@outlook.com>
2020-10-15 11:45:50 +0300

    View added

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<html>
+<h1>This is the new and improved guessing game</h1>
+<p>You need to guess a number. If correct, the application
+will give a message below.</p>
+
+<form method="post">
+<p><label for="guess">Enter your guess</label>
+<input type="text" name="guess" id="guess">
+<input type="submit" value="Guess">
+</p>
+</form>
+</html>
